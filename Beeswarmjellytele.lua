c6f12d1e8943be1ad78f07bfb51d8850671ba4b5
denisser121 <61875961+denisbeyourself@users.noreply.github.com>
2022-03-19 15:10:09 +0200

    Create Beeswarmjellytele.lua

---
+++ Beeswarmjellytele.lua
@@ -0,0 +1,16 @@
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-233, 44, 417)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-64, 37, 113)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(314, 63, 213)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(216, 34, -28)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(111, 65, -59)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(138, 65, 258)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-188, 65, 366)
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(34, 59, 190)
